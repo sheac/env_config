4407510c9862785b1a419ac81a68452da5a4566e
Shealen Clare <shealen@YMCs-Mac-mini-7.local>
2013-10-10 16:25:27 -0700

    Make deploy.sh reload .bashrc

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# copy files to their proper locations
 cp ./.bashrc ~/.bashrc
 cp ./.vimrc ~/.vimrc
+
+# reload the .bashrc file
+source ~/.bashrc
+
+# cleanup old files in improper locations
+# (none yet)
